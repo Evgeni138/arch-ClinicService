04194eddd2d116848686fc5ccbfed39133b49af3
Evgegeni <popovevgeni81@gmail.com>
2023-10-05 14:47:05 +0800

    Clinic_Desctop

---
+++ ClinicaDesktop/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ClinicaDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnUpdate = new Button();
+            listViewClients = new ListView();
+            columnHeaderId = new ColumnHeader();
+            columnHeaderSurName = new ColumnHeader();
+            columnHeaderFirstName = new ColumnHeader();
+            columnHeaderЗPatronymic = new ColumnHeader();
+            SuspendLayout();
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.BackColor = SystemColors.Highlight;
+            btnUpdate.ForeColor = SystemColors.ButtonHighlight;
+            btnUpdate.Location = new Point(649, 381);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(139, 57);
+            btnUpdate.TabIndex = 0;
+            btnUpdate.Text = "Обновить";
+            btnUpdate.UseMnemonic = false;
+            btnUpdate.UseVisualStyleBackColor = false;
+            btnUpdate.Click += btn_Click;
+            // 
+            // listViewClients
+            // 
+            listViewClients.Columns.AddRange(new ColumnHeader[] { columnHeaderId, columnHeaderSurName, columnHeaderFirstName, columnHeaderЗPatronymic });
+            listViewClients.FullRowSelect = true;
+            listViewClients.GridLines = true;
+            listViewClients.Location = new Point(12, 12);
+            listViewClients.MultiSelect = false;
+            listViewClients.Name = "listViewClients";
+            listViewClients.Size = new Size(776, 363);
+            listViewClients.TabIndex = 1;
+            listViewClients.UseCompatibleStateImageBehavior = false;
+            listViewClients.View = View.Details;
+            // 
+            // columnHeaderId
+            // 
+            columnHeaderId.Text = "#";
+            // 
+            // columnHeaderSurName
+            // 
+            columnHeaderSurName.Text = "Фамилия";
+            columnHeaderSurName.Width = 200;
+            // 
+            // columnHeaderFirstName
+            // 
+            columnHeaderFirstName.Text = "Имя";
+            columnHeaderFirstName.Width = 200;
+            // 
+            // columnHeaderЗPatronymic
+            // 
+            columnHeaderЗPatronymic.Text = "Отчество";
+            columnHeaderЗPatronymic.Width = 200;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listViewClients);
+            Controls.Add(btnUpdate);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Моя клиника";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnUpdate;
+        private ListView listViewClients;
+        private ColumnHeader columnHeaderId;
+        private ColumnHeader columnHeaderSurName;
+        private ColumnHeader columnHeaderFirstName;
+        private ColumnHeader columnHeaderЗPatronymic;
+    }
+}
